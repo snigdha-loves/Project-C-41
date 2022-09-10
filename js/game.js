2325934911ae644999f40a5c1b92e8aa46d56e47
snigdha-loves <90294581+snigdha-loves@users.noreply.github.com>
2022-09-10 12:54:32 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -132,10 +132,7 @@ class Game{
                       }
 
                       if(obstacleGroup.isTouching(players)){
-                       // gameState = 0;
-                       // gameState = 1;
                         gameState = 2;
-                       // gameState = 3;
                       }
                   }
                 }
@@ -166,19 +163,20 @@ class Game{
 
 
     addObstacles()
-    {       
+    {
+       
             var x, y;
-            //x= 200;
+      
             x = random(0, width-100);
-            //x = random(0);
-            //x = random();
-            
-            
             y = 0
-            var obstacle = createSprite(x, y);
-            obstacle.addImage("obstacle", obstacleImage);
-            obstacle.velocityY = 4;
-            obstacle.scale = 0.15;
-            obstacleGroup.add(obstacle);
+                var obstacle = createSprite(x, y);
+                obstacle.addImage("obstacle", obstacleImage);
+                obstacle.velocityY = 4;
+          
+                obstacle.scale = 0.15;
+                obstacleGroup.add(obstacle);
+            
+           
+          
     }
 }
